23aea1992eaa6300ee085a8d4e2fbb2e5565e753
Nguyen Hoan <hoan.nv.bk63@gmail.com>
2021-04-13 20:09:52 +0700

    Lab07

---
+++ Lab07/AimsProject/src/hust/soict/hedspi/aims/MemoryDaemon.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hust.soict.hedspi.aims;
+
+
+public class MemoryDaemon implements Runnable {
+    long memoryUsed = 0;
+    @Override
+    public void run() {
+        Runtime rt = Runtime.getRuntime();
+        long used;
+        
+        while(true) {
+            used = rt.totalMemory() - rt.freeMemory();
+            if(used != memoryUsed) {
+                System.out.println("\tMemory used = " + used);
+                memoryUsed = used;
+            }
+        }
+    }
+}
---
+++ Lab07/AimsProject/src/hust/soict/hedspi/aims/media/Media.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hust.soict.hedspi.aims.media;
+
+public abstract class Media {
+    protected String title;
+    protected String category;
+    protected float cost;
+    public Media() {
+    }
+
+    public Media(String title) {
+        this.title = title;
+    }
+
+    public Media(String title, String category) {
+        this.title = title;
+        this.category = category;
+    }
+
+    public Media(String title, String category, float cost) {
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+    
+    public abstract void print();
+}
